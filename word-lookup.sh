fc111c2f921157a20d657506aa54a07f52c06725
Alexander G. Nielsen <algn@algn.dk>
2021-02-26 18:36:02 +0100

    Added script

---
+++ word-lookup.sh
@@ -0,0 +1,7 @@
+word=$(xclip -o)
+res=$(curl -s "https://api.dictionaryapi.dev/api/v2/entries/en_US/$word")
+regex=$'"definition":"\K(.*?)(?=")'
+definitions=$(echo $res | grep -Po "$regex")
+separatedDefinition=$(sed ':a;N;$!ba;s/\n/\n\n/g' <<< "$definitions")
+
+notify-send -u low -t 10000 "$word" "$separatedDefinition"
