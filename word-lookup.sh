6a76b6c79fdd9fa89407efc98512877fafe5663e
fwsmit <fw.smit01@gmail.com>
2022-04-12 20:18:10 +0200

    Add usage

--- word-lookup.sh
+++ word-lookup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+usage(){
+	echo "Usage: $(basename "$0") [-h]
+        Looks up the definition of currently selected word."
+}
+
+while getopts 'h' c
+do
+	case $c in
+		h) usage; exit ;;
+		*) usage; exit 1 ;;
+	esac
+done
+
 word=$(xclip -o)
 res=$(curl -s "https://api.dictionaryapi.dev/api/v2/entries/en_US/$word")
 regex=$'"definition":"\K(.*?)(?=")'
