1f5446952947edb1097247f4678bf541a431f2cd
fwsmit <fw.smit01@gmail.com>
2022-04-14 10:37:40 +0200

    Let notification priority default to normal

--- word-lookup.sh
+++ word-lookup.sh
@@ -31,4 +31,4 @@ res=$(curl -s "https://api.dictionaryapi.dev/api/v2/entries/en_US/$word")
 regex=$'"definition":"\K(.*?)(?=")'
 definitions=$(echo "$res" | grep -Po "$regex")
 separatedDefinition=$(sed ':a;N;$!ba;s/\n/\n\n/g' <<< "$definitions")
-notify-send -u low -a "word-lookup" -t 10000 "$word" "$separatedDefinition"
+notify-send -a "word-lookup" -t 10000 "$word" "$separatedDefinition"
